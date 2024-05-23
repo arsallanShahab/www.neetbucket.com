7915df67e2afc883afa95ed2761e159c9b813ba1
Arsallan Shahab <mdarsallan@gmail.com>
2024-05-23 17:59:06 +0530

    chore: Update @clerk/nextjs dependency to version 5.1.0 and refactor middleware paths

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,24 +1,29 @@
-import { authMiddleware } from "@clerk/nextjs";
-
 // This example protects all routes including api/trpc routes
 // Please edit this to allow other routes to be public as needed.
 // See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your Middleware
-export default authMiddleware({
-  publicRoutes: [
-    // "/",
-    // "/soft-copy",
-    // "/hard-copy",
-    // "/contact",
-    // "/about",
-    // "/faq",
-    // "/privacy-policy",
-    // "/terms-and-conditions",
-    // "/sign-in",
-    // "/sign-up",
-    // "/api/webhook(.*)",
-  ],
+
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+
+const isPublicRoute = createRouteMatcher([
+  "/",
+  "/soft-copy",
+  "/hard-copy",
+  "/contact",
+  "/about",
+  "/faq",
+  "/privacy-policy",
+  "/terms-and-conditions",
+  "/sign-in",
+  "/sign-up",
+  "/api/webhook(.*)",
+]);
+
+export default clerkMiddleware((auth, request) => {
+  if (!isPublicRoute(request)) {
+    auth().protect();
+  }
 });
 
 export const config = {
-  matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
+  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
 };
