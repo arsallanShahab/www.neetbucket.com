b53620a15f757e71957b218c67cf0c1abd62960c
Arsallan Shahab <mdarsallan@gmail.com>
2024-05-23 15:04:13 +0530

    refactor: Remove profile slice from rootReducer

--- src/redux/rootReducer.ts
+++ src/redux/rootReducer.ts
@@ -1,10 +1,8 @@
 // src/redux/rootReducer.ts
 import { combineReducers } from "@reduxjs/toolkit";
 import { cartReducer } from "./slices/cart";
-import { profileReducer } from "./slices/profile";
 const rootReducer = combineReducers({
   cart: cartReducer,
-  profile: profileReducer,
 });
 
 export default rootReducer;
