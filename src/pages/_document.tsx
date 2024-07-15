6afa59e501ee949164c7d1b3d1aee516dfea04ed
Arsallan Shahab <mdarsallan@gmail.com>
2024-07-15 15:38:03 +0530

    add google adsense code

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,6 +5,7 @@ import Document, {
   Main,
   NextScript,
 } from "next/document";
+import Script from "next/script";
 
 class MyDocument extends Document {
   static async getInitialProps(ctx: DocumentContext) {
@@ -20,6 +21,11 @@ class MyDocument extends Document {
             name="google-adsense-account"
             content="ca-pub-5096956769841020"
           ></meta>
+          <Script
+            strategy="afterInteractive"
+            async
+            src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5096956769841020"
+          />
         </Head>
         <body>
           <Main />
