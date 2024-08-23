7e7148e1e77de8c4929a2a42eda4801469837cd1
Arsallan Shahab <mdarsallan@gmail.com>
2024-08-23 19:02:12 +0530

    added coupon code feature

--- src/app/api/coupons/apply/route.ts
+++ src/app/api/coupons/apply/route.ts
@@ -6,32 +6,37 @@ export async function GET(req: NextRequest) {
     const code = req.nextUrl.searchParams.get("code");
     console.log(code, "code");
     if (!code) {
-      return NextResponse.json({
-        success: false,
-        message: "Invalid coupon code",
-      });
+      return Response.json(
+        {
+          success: false,
+          message: "Please provide a coupon",
+        },
+        {
+          status: 400,
+        },
+      );
     }
     const { db } = await connectToDatabase();
     const coupons = await db.collection("coupons").findOne({
       code: code,
     });
     if (!coupons) {
-      return NextResponse.json({
+      return Response.json({
         success: false,
-        message: "Invalid coupon code",
+        message: "Coupon not found",
       });
     }
-    return NextResponse.json({
+    return Response.json({
       success: true,
+      message: "Coupon found",
       data: coupons,
-      message: "Coupon applied successfully",
     });
   } catch (error) {
     console.log(error, "error");
-    return NextResponse.json({
+    return Response.json({
       success: false,
-      message: "Something went wrong",
       error: error,
+      message: "Something went wrong",
     });
   }
 }
